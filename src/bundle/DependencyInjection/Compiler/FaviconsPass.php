c2144d4cca8d4cbc10e320ebd265aaeed6129633
Emmanuel DROUET <emmanuel.drouet@gmail.com>
2018-05-06 20:55:12 +0200

    create var favicons directory when loading favicons bundle to be included in twig path

--- src/bundle/DependencyInjection/Compiler/FaviconsPass.php
+++ src/bundle/DependencyInjection/Compiler/FaviconsPass.php
@@ -11,6 +11,10 @@ class FaviconsPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $faviconsViewPath = $container->getParameter('kernel.root_dir') . '/../web/' . $container->getParameter('ezsettings.admin_group.var_dir') . '/' . $container->getParameter('ezsettings.default.storage_dir') . '/images/' . Generator::FAVICONS_DIR;
+        if (!is_dir($faviconsViewPath)) {
+            mkdir($faviconsViewPath);
+        }
+
         $container->getDefinition('twig.loader.filesystem')->addMethodCall('addPath', [$faviconsViewPath]);
     }
 }
